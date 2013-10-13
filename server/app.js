cf60c71c45d4a772388e782c7cc02c34982220de
George Allison <allisg1@umbc.edu>
2013-10-13 00:40:38 -0400

    switched from bcrypt to scrypt

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ var express = require('express')
 , LocalStrategy = require('passport-local').Strategy
 , mongodb = require('mongodb')
 , mongoose = require('mongoose')
-, bcrypt = require('bcrypt')
-, SALT_WORK_FACTOR = 10;
+, scrypt = require('scrypt');
 
 mongoose.connect('localhost', 'test');
 var db = mongoose.connection;
@@ -23,26 +22,22 @@ var userSchema = mongoose.Schema({
   accessToken: { type: String } // Used for Remember Me
 });
 
-// Bcrypt middleware
+// Scrypt middleware
 userSchema.pre('save', function(next) {
 	var user = this;
 
 	if(!user.isModified('password')) return next();
 
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if(err) return next(err);
-
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			if(err) return next(err);
-			user.password = hash;
-			next();
-		});
-	});
+  scrypt.hash(user.password, 0.5, function(err, hash) {
+    if(err) return next(err);
+    user.password = hash;
+    next();
+  });
 });
 
 // Password verification
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
-	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+	scrypt.verifyHash(this.password, candidatePassword, function(err, isMatch) {
 		if(err) return cb(err);
 		cb(null, isMatch);
 	});
